40a8718fad8fe5abb518d0b7e8dbb029869dad5e
Divya Prakash Jha <prakashdivya0607@gmail.com>
2022-12-03 03:37:09 +0530

    Updated Assignment-6

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -24,7 +24,7 @@ public class ExceptionHandling {
 
 	public static void main(String[] args) {
 		
-		int op = 0, n = 0, x = 0, y = 0, ans = 0, i = 0, j = 0;
+		int op = 0, n = 0, x = 0, y = 0, ans = 0, i = 0, j = 0, num = 0, den = 0;
 		int[] arr = new int[0];
 		Boolean flag = false;
 		
@@ -32,39 +32,23 @@ public class ExceptionHandling {
 		
 		do {
 			System.out.println("\n\nChoose the desired option : ");
-			System.out.println("1. Take input for division");
-			System.out.println("2. Perform division");
-			System.out.println("3. Create an array");
-			System.out.println("4. Input array element");
-			System.out.println("5. Access array element");
+			System.out.println("1. Create an Array");
+			System.out.println("2. Input Array element"); // For number format exception
+			System.out.println("3. Access Array element"); // For array index out of bound exception
+			System.out.println("4. Enter the Index of Array for Division"); // For number format and array index out of bound exception 
+			System.out.println("5. Perform Division"); // For arithmetic exception
 			System.out.println("*. Press any other numeric key to exit");
 			
 			op = E.takeInput();
 			
 			switch(op) {
 			case 1:
-				System.out.println("Enter the value of x : ");
-				x = E.takeInput();
-				System.out.println("Enter the value of y : ");
-				y = E.takeInput();
-				break;
-			case 2:
-				try {
-					ans = x/y;
-					System.out.print("x/y = "+ ans);
-				}
-				catch(ArithmeticException e) {
-					System.out.println(e);
-					System.out.println("Cannot divide by 0");
-				}
-				break;
-			case 3:
 				flag = true;
 				System.out.println("Enter the size of the array : ");
 				n = E.takeInput();
 				arr = new int[n];
 				break;
-			case 4:
+			case 2:
 				if (!flag) {
 					System.out.println("Please create an array first!!");
 				}
@@ -82,7 +66,7 @@ public class ExceptionHandling {
 					}
 				}
 				break;
-			case 5:
+			case 3:
 				if (!flag) {
 					System.out.println("Please create an array first!!");
 				}
@@ -98,8 +82,43 @@ public class ExceptionHandling {
 					}
 				}
 				break;
+			case 4:
+				if (!flag) {
+					System.out.println("Please create an array first!!");
+				}
+				else {
+					System.out.println("Enter the index of numerator : ");
+					x = E.takeInput();
+					try {
+						num = arr[x];
+					}
+					catch(ArrayIndexOutOfBoundsException e) {
+						System.out.println(e);
+					}
+					
+					System.out.println("Enter the index of denomenator : ");
+					y = E.takeInput();
+					try {
+						den = arr[y];
+					}
+					catch(ArrayIndexOutOfBoundsException e) {
+						System.out.println(e);
+					}	
+				}
+				break;
+			case 5:
+				try {
+					ans = num/den;
+					System.out.print("division = "+ ans);
+				}
+				catch(ArithmeticException e) {
+					System.out.println(e);
+					System.out.println("Cannot divide by 0");
+				}
+				break;
 			default:
 				op = 0;
+				break;
 			}
 			
 		}while(op!=0);
