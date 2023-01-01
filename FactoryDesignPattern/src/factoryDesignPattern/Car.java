5a83875202852eab063d8b861ada0ee2b527a972
Divya Prakash Jha <91480811+DivyaPrakashJha@users.noreply.github.com>
2023-01-02 00:59:32 +0530

    Delete FactoryDesignPattern/src/factoryDesignPattern directory

--- FactoryDesignPattern/src/factoryDesignPattern/Car.java
+++
@@ -1,9 +0,0 @@
-package factoryDesignPattern;
-
-public interface Car {
-	abstract void carType(); // For final display
-	
-	abstract void gatherParts();
-	abstract void assembleParts();
-	abstract void ready();
-}
